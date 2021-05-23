c7d2b1a82ddd8535414541823aa3c10815fbbb93
khoahuynhdev <khoa0319@gmail.com>
2021-05-23 19:50:01 +0700

    add visitor pattern

---
+++ visitor-pattern-oop.ts
@@ -0,0 +1,71 @@
+// This implementation of Visitor pattern in built on double dispatch principle
+
+// Element to visit 
+interface DocumentItem {
+    accept: (visitor: Visitor) => void
+}
+
+class Paragraph implements DocumentItem {
+    accept(visitor: Visitor) {
+        visitor.renderParagraph(this)
+    }
+}
+
+class Picture implements DocumentItem {
+    accept(visitor: Visitor) {
+        visitor.renderPicture(this)
+    }
+}
+
+class Table implements DocumentItem {
+    accept(visitor: Visitor) {
+        visitor.renderTable(this)
+    }
+}
+
+// visitors
+interface Visitor {
+    renderParagraph: (paragraph: Paragraph) => void;
+    renderPicture: (picture: Picture) => void;
+    renderTable: (table: Table) => void;
+}
+
+class Renderer implements Visitor {
+    renderParagraph(paragraph: Paragraph) {
+        console.log('render paragraph')
+    }
+    renderPicture(picture: Picture) {
+        console.log('render picture')
+    }
+    renderTable(table: Table) {
+        console.log('render table')
+    }
+}
+
+class ScreenReader implements Visitor {
+    renderParagraph(paragraph: Paragraph) {
+        console.log('read paragraph')
+    }
+    renderPicture(picture: Picture) {
+        console.log('read picture')
+    }
+    renderTable(table: Table) {
+        console.log('read table')
+    }
+}
+let docs: Array<DocumentItem> = [
+    new Table(),
+    new Paragraph(),
+    new Picture(),
+]
+
+const visitor: Visitor = new ScreenReader();
+
+for (let d of docs) {
+    d.accept(visitor);
+}
+// m - visitors
+// n - elements
+// +1 n => update m
+// +1 m => unchange n
+// n must know about what process them => not seperation of concern
